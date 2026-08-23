1029161e221cff922ab2cce577d5d2f73ae58402
T-Luxshan <luckybraveboys@gmail.com>
2026-08-23 19:56:26 +0530

    validator for the URL

---
+++ backend/linkshort/src/main/java/com/luxshan/linkshort/linkshort/util/UrlValidator.java
@@ -0,0 +1,21 @@
+package com.luxshan.linkshort.linkshort.util;
+
+import org.springframework.stereotype.Component;
+@Component
+public class UrlValidator {
+    public boolean isValid(String url) {
+        if(url == null || url.isEmpty()) {
+            return false;
+        }
+        // if(url.length() > 2048) {
+        //     return false;
+        // }
+        if(url.startsWith("http://") || url.startsWith("https://")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
