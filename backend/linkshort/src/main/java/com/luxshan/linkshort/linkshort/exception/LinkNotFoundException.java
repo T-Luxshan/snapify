47d43f12b1a04282a611869139d63e9c43acb923
T-Luxshan <luckybraveboys@gmail.com>
2026-08-23 19:56:41 +0530

    exception for link not found

---
+++ backend/linkshort/src/main/java/com/luxshan/linkshort/linkshort/exception/LinkNotFoundException.java
@@ -0,0 +1,8 @@
+package com.luxshan.linkshort.linkshort.exception;
+
+public class LinkNotFoundException extends RuntimeException {
+    public LinkNotFoundException(String message) {
+        super(message);
+    }
+
+}
