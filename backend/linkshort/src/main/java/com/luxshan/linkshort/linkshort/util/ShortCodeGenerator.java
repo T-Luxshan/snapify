28466b7513d6d417e017fa7405b5c8f38112bcb2
T-Luxshan <luckybraveboys@gmail.com>
2026-08-23 19:55:53 +0530

    shortcode generator for URL

---
+++ backend/linkshort/src/main/java/com/luxshan/linkshort/linkshort/util/ShortCodeGenerator.java
@@ -0,0 +1,20 @@
+package com.luxshan.linkshort.linkshort.util;
+
+import org.springframework.stereotype.Component;
+import java.util.Random;
+
+@Component
+public class ShortCodeGenerator {
+ final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+ public String generate() {
+    StringBuilder shortCode = new StringBuilder();
+    Random random = new Random();
+    for (int i = 0; i < 7; i++) {
+        shortCode.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+    }
+    return shortCode.toString();
+ }
+}
+
+
