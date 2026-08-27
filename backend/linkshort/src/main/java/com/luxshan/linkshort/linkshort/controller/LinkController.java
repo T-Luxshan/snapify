0a44f663823882244a0231d7685c7d1576479c56
T-Luxshan <luckybraveboys@gmail.com>
2026-08-27 22:03:37 +0530

    prefix path to LinkController

--- backend/linkshort/src/main/java/com/luxshan/linkshort/linkshort/controller/LinkController.java
+++ backend/linkshort/src/main/java/com/luxshan/linkshort/linkshort/controller/LinkController.java
@@ -5,9 +5,11 @@ import com.luxshan.linkshort.linkshort.exception.LinkNotFoundException;
 import com.luxshan.linkshort.linkshort.service.LinkService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/links")
 public class LinkController {
 
     private final LinkService linkService;
