07183091dcf14445c40fe662e202e76a2d67a9ea
T-Luxshan <luckybraveboys@gmail.com>
2026-08-27 21:58:21 +0530

    Get link endpoint written

---
+++ backend/linkshort/src/main/java/com/luxshan/linkshort/linkshort/controller/LinkController.java
@@ -0,0 +1,28 @@
+package com.luxshan.linkshort.linkshort.controller;
+
+import com.luxshan.linkshort.linkshort.dto.LinkResponse;
+import com.luxshan.linkshort.linkshort.exception.LinkNotFoundException;
+import com.luxshan.linkshort.linkshort.service.LinkService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LinkController {
+
+    private final LinkService linkService;
+
+    public LinkController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @GetMapping("/{shortCode}")
+    public LinkResponse getLink(@PathVariable String shortCode){
+        return linkService.getLink(shortCode);
+    }
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
