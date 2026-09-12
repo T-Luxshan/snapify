1762e451e94edc60252e5cf859b2241251eb25de
T-Luxshan <luxshan.thuraisingam@gmail.com>
2026-09-12 21:44:50 +0530

    builded added

--- backend/snapify/src/main/java/com/luxshan/snapify/dto/CachedLink.java
+++ backend/snapify/src/main/java/com/luxshan/snapify/dto/CachedLink.java
@@ -1,6 +1,7 @@
 package com.luxshan.snapify.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CachedLink {
 
     private String originalUrl;
