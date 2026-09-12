24f435ef9d61628d8ed9ab3daf0898221c830d19
T-Luxshan <luxshan.thuraisingam@gmail.com>
2026-09-12 21:11:34 +0530

    Redis service

---
+++ backend/snapify/src/main/java/com/luxshan/snapify/service/RedisService.java
@@ -0,0 +1,20 @@
+package com.luxshan.snapify.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public void set(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
